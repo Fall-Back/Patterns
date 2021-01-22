0a0d923a7c65c7fd3c46e210569ea0fff82208d1
Andy Kirk <andy@andykirk.net>
2021-01-22 16:17:34 +0000

    Update content-min-row-v1.js

--- Content Min Row/js/content-min-row-v1.js
+++ Content Min Row/js/content-min-row-v1.js
@@ -14,8 +14,8 @@
 
 (function() {
 
-    var debug                                = true;
-    //var debug                                = false;
+    //var debug                                = true;
+    var debug                                = false;
     var ident                                = 'cmr';
     var selector                             = '[data-js="' + ident + '"]';
     var js_classname_prefix                  = 'js';
